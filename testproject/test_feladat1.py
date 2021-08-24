383457fa97d736f9673c38e988466e8eb77ece73
panniangyan <panniangyan@gmail.com>
2021-08-24 17:19:03 +0200

    feladat1 class

--- testproject/test_feladat1.py
+++ testproject/test_feladat1.py
@@ -2,39 +2,47 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+class TestFeladat1(object):
+    def setup(self):
+        self.browser = webdriver.Chrome("/usr/bin/chromedriver")
+        self.browser.get("http://webshop.progmasters.hu")
 
-def test_hogwards():
+    def teardown(self):
+        self.browser.quit()
 
-# feladat URL-je
-    URL = "https://witty-hill-0acfceb03.azurestaticapps.net/hogwards.html"
 
-# oldal betöltése
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser.maximize_window()
-    browser.get(URL)
+    def test_hogwards(self):
+
+    # feladat URL-je
+        URL = "https://witty-hill-0acfceb03.azurestaticapps.net/hogwards.html"
+
+    # oldal betöltése
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser.maximize_window()
+        self.browser.get(URL)
 
 # # utas adatai
-    passenger_name = "Harry Potter"
-    departure_date = "08/09/2021"
-    departure_time = "0800AM"
+        passenger_name = "Harry Potter"
+        departure_date = "08/09/2021"
+        departure_time = "0800AM"
 
-    browser.find_element_by_id("passenger").send_keys(passenger_name)
-    browser.find_element_by_id("departure-date").send_keys(departure_date)
-    browser.find_element_by_id("departure-time").send_keys(departure_time)
+        self.browser.find_element_by_id("passenger").send_keys(passenger_name)
+        self.browser.find_element_by_id("departure-date").send_keys(departure_date)
+        self.browser.find_element_by_id("departure-time").send_keys(departure_time)
 
-    issue_ticket_btn = browser.find_element_by_id("issue-ticket").click()
+        issue_ticket_btn = self.browser.find_element_by_id("issue-ticket").click()
 
-    time.sleep(2)
+        time.sleep(2)
 
-    departure_date = "2021-08-09"
-    departure_time = "08:00AM"
-
-
-    assert browser.find_element_by_id("passenger-name").text == passenger_name.upper()
-    assert browser.find_element_by_id("departure-date-text").text == departure_date
-    assert browser.find_element_by_id("side-detparture-date").text == departure_date
-    assert browser.find_element_by_id("departure-time-text").text == departure_time
-    assert browser.find_element_by_id("side-departure-time").text == departure_time
+        departure_date = "2021-08-09"
+        departure_time = "08:00AM"
 
 
-    browser.quit()
+        assert self.browser.find_element_by_id("passenger-name").text == passenger_name.upper()
+        assert self.browser.find_element_by_id("departure-date-text").text == departure_date
+        assert self.browser.find_element_by_id("side-detparture-date").text == departure_date
+        assert self.browser.find_element_by_id("departure-time-text").text == departure_time
+        assert self.browser.find_element_by_id("side-departure-time").text == departure_time
+
+
+
